56de727bb676ddc8e8911f52614ec9146db16493
xwhoknowz <jeremiah2498@gmail.com>
2024-01-10 23:17:47 -0600

    added mobile responsiveness and finished logic for saving user entry to local storage

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# weather-dashboard
-An active weather widget for displaying the 5 day forecast for a requested city.
+# Weather Dashboard
+## Description
+
+The 
+
+## Installation
+
+N/A
+
+## Usage
+
+Upon page load up the user see the start page, they can begin by pressing the start quiz button. Upon doing so the start page is hidden and the first question is loaded. The user will continue to progress the quiz by clicking one of the four buttons that corresponds to each question. After the final question is answered or when the timer runs out the user is taken to the score board page where they are able to record intials in a form which is then saved to the browser alongside their final time.
+
+## Credits
+
+Thank you to Leif, Harley, Thomas, and Daniel for  
+
+## License
+
+Refer to the liscense in the GitHub repo.
+
+## Links
+
+Pop Quiz Website: [https://xwhoknowz.github.io/Pop-Quiz/]
+
+## Website Sample
+
+![Pop Quiz website sample image](./assets/img/Pop_Quiz.jpg).
+
--- assets/script.js
+++ assets/script.js
@@ -1,68 +1,129 @@
 var weatherKey = `03c94a8f5d7f90ec48a9d32b889715d8`;
 var formEl = document.querySelector(`#userInput`)
+var ulEl = document.querySelector(`.buttons`)
 var cityInput = document.querySelector(`#cities`);
+var searchedCity;
+var cityButtons = [];
 var forecastArray = [];
 var cardDate = document.querySelectorAll(`.date`)
 var cardWeather = document.querySelectorAll(`.weather`)
 var cardTemp = document.querySelectorAll(`.temp`)
 var cardWind = document.querySelectorAll(`.wind`)
 var cardHumid = document.querySelectorAll(`.humid`)
+var currentDate = document.getElementById(`date`)
+var currentWeather = document.getElementById(`weather`)
+var currentTemp = document.getElementById(`temp`)
+var currentWind = document.getElementById(`wind`)
+var currentHumid = document.getElementById(`humid`)
+var btnEl;
 
 
 function citySearch(event) {
-    event.preventdefault();
-    
-    var searchedCity = cityInput.value.trim()
-    console.log(searchedCity);
-    
+    event.preventDefault();
+
+    searchedCity = cityInput.value.trim()
+    // console.log(searchedCity);
+
+    fiveDayForecast()
+    buttonMaker()
+    buttonSaver()
 }
 
 
 
-function fiveDayForecast(cities) {
-    cityName = `Dallas`;
-    var dataRequest = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${weatherKey}`
-    
+function fiveDayForecast() {
+    cityName = searchedCity;
+    var dataRequest = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${weatherKey}&units=imperial`
+
     fetch(dataRequest)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-        var lat = data.coord.lat;
-        var lon = data.coord.lon;
-        var fiveDayRequest = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherKey}&units=imperial`
-        
-        fetch(fiveDayRequest)
-        .then(function (response2) {
-            console.log(response2);
-            return response2.json()
+        .then(function (response) {
+            return response.json();
         })
-        .then(function (data2) {
-            console.log(data2);
-            for (let i = 0; i < data2.list.length; i++) {
-                if(data2.list[i].dt_txt.includes(`12:00:00`)){
-                    forecastArray.push(data2.list[i])
-                    console.log(data2.list[i]);
-                    console.log(forecastArray);
-                }
-            }
-            for (let i = 0; i < forecastArray.length; i++) {
-                var forecastDate = forecastArray[i].dt_txt.split(` `)
-                var weatherIcon = forecastArray[i].weather[0].icon + `.png`
-                cardDate[i].textContent = forecastDate[0]
-                cardWeather[i].textContent =  `https://openweathermap.org/img/wn/${weatherIcon}`
-                cardTemp[i].textContent = `Temp: ` + forecastArray[i].main.temp + ` °F`
-                cardWind[i].textContent = `Wind: ` + forecastArray[i].wind.speed + ` MPH`
-                cardHumid[i].textContent = `Humidity: ` + forecastArray[i].main.humidity + `%`
-                
-            }
-            
-            
-            
+        .then(function (data) {
+            console.log(data);
+            var lat = data.coord.lat;
+            var lon = data.coord.lon;
+            var fiveDayRequest = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherKey}&units=imperial`
+            // var currentDate = data.dt_txt.split(` `)
+            var currentWeatherIcon = data.weather[0].icon + `.png`
+
+            // currentDate.textContent = 
+            currentWeather.setAttribute(`src`, `https://openweathermap.org/img/wn/${currentWeatherIcon}`)
+            currentTemp.textContent = `Temp: ` + data.main.temp + ` °F`
+            currentWind.textContent = `Wind: ` + data.wind.speed + ` MPH`
+            currentHumid.textContent = `Humidity: ` + data.main.humidity + `%`
+
+
+            fetch(fiveDayRequest)
+                .then(function (responseToo) {
+                    // console.log(responseToo);
+                    return responseToo.json()
+                })
+                .then(function (dataToo) {
+                    console.log(dataToo);
+                    forecastArray = [];
+                    for (let i = 0; i < dataToo.list.length; i++) {
+                        if (dataToo.list[i].dt_txt.includes(`12:00:00`)) {
+                            forecastArray.push(dataToo.list[i])
+                            // console.log(dataToo.list[i]);
+                            // console.log(forecastArray);
+                        }
+                    }
+                    console.log(cardDate);
+                    console.log(forecastArray.length);
+                    for (let i = 0; i < forecastArray.length; i++) {
+                        // console.log(`This is FA`, forecastArray[i]);
+                        var forecastDate = forecastArray[i].dt_txt.split(` `)
+                        var weatherIcon = forecastArray[i].weather[0].icon + `.png`
+                        if (cardDate) {
+                            // console.log(cardDate[i]);
+                            cardDate[i].textContent = forecastDate[0]
+                        }
+                        cardWeather[i].setAttribute(`src`, `https://openweathermap.org/img/wn/${weatherIcon}`)
+                        cardTemp[i].textContent = `Temp: ` + forecastArray[i].main.temp + ` °F`
+                        cardWind[i].textContent = `Wind: ` + forecastArray[i].wind.speed + ` MPH`
+                        cardHumid[i].textContent = `Humidity: ` + forecastArray[i].main.humidity + `%`
+
+                    }
+
+
+
+                })
         })
-    })
 }
 
-fiveDayForecast()
+function buttonMaker() {
+    console.log(searchedCity);
+    cityButtons.push(searchedCity)
+    console.log(cityButtons);
+
+    btnEl = document.createElement(`button`)
+    btnEl.setAttribute(`class`, `btn, btn-secondary`)
+
+    
+    for (let i = 0; i < cityButtons.length; i++) {
+        
+        btnEl.textContent = cityButtons[i]
+        ulEl.appendChild(btnEl)
+            
+    }
+    
+    
+}
+function buttonSaver() {
+   localStorage.setItem(`cityButtons`, JSON.stringify(cityButtons)) 
+    
+}
+
+var savedButtons = JSON.parse(localStorage.getItem(`cityButtons`))
+if(savedButtons !== null){
+    for (let i = 0; i < cityButtons.length; i++) {
+        
+      
+            
+    }
+}
+
+
+formEl.addEventListener(`submit`, citySearch)
 
